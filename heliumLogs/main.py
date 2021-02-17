3b3816a5facebd2b8da5d6a4749c2c7dd2505530
MrMeowlnir <57915600+MrMeowlnir@users.noreply.github.com>
2021-02-18 00:06:25 +0300

    Доработки версия 0.5
    Добавлен родительский класс LogFile для всех типов лог-файлов
    Ускорено заполнение таблицы с результатом путем создания модели датафрейма и подключения ее к QTableView
    Добавлены классы файлов температуры и уровня
    Доработана логика сборки всех данных в одну общую таблицу

--- heliumLogs/main.py
+++ heliumLogs/main.py
@@ -13,7 +13,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import pyqtgraph as pg
-from PyQt5 import uic, QtCore
+from PyQt5 import uic, QtCore, QtGui
 from PyQt5.QtWidgets import (QMainWindow, QTableWidgetItem,
     QFileDialog, QApplication, QComboBox, QLabel)
 
@@ -26,8 +26,34 @@ def dateConcat(df):
     dataframe.drop(['Time', 'Date'], axis=1, inplace=True)
     return dataframe
 
-""" Класс Лог Давления"""
-class Pressure():
+
+class PandasModel(QtCore.QAbstractTableModel):
+    """
+    Class to populate a table view with a pandas dataframe
+    """
+    def __init__(self, data, parent=None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if index.isValid():
+            if role == QtCore.Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
+
+""" Родительский класс ЛогФайл"""
+class LogFile():
     def __init__(self, fnames = ''):
         if fnames == '':
             self.fnames = []
@@ -35,7 +61,7 @@ class Pressure():
             self.fnames = fnames
             
     def set_fnames(self):
-        fname = QFileDialog.getOpenFileName(filter = 'Text Files (*.txt)')[0]
+        fname = QFileDialog.getOpenFileName()[0]
         if fname != '':
             self.fnames = [fname]
         else:
@@ -49,9 +75,14 @@ class Pressure():
         return 0
     
     def AddFName(self):
-        fname = QFileDialog.getOpenFileName(filter = 'Text Files (*.txt)')[0]
+        fname = QFileDialog.getOpenFileName()[0]
         if fname != '':
             self.fnames.append(fname)
+
+""" Класс Лог Давления"""
+class Pressure(LogFile):
+    def __init__(self):
+        super().__init__()
     
     def GetDataFrame(self):
         while self.fnames == [] or self.fnames == '':
@@ -79,6 +110,51 @@ class Pressure():
                 pass
         
         return result
+
+""" Класс температурного лога"""
+class Temperature(LogFile):
+    def __init__(self):
+        super().__init__()
+    
+    def GetDataFrame(self):
+        while self.fnames == [] or self.fnames == '':
+            self.set_fnames()
+        
+        
+        names = ["T1, K", "T2, K", "T3, K", "T4, K", "T5, K", "T6, K", "T7, K","T8, K","Date", "Time"]
+        result = pd.DataFrame()
+        for filename in self.fnames:
+            try:
+                dataframe = pd.read_csv(filename, delimiter='\t', names=names)
+                dataframe = dateConcat(dataframe)
+                result = result.append(dataframe, ignore_index = True)          
+            except:
+                pass
+        
+        return result
+
+
+""" Класс температурного лога"""
+class Level(LogFile):
+    def __init__(self):
+        super().__init__()
+    
+    def GetDataFrame(self):
+        while self.fnames == [] or self.fnames == '':
+            self.set_fnames()
+        
+        names = ["Level, cm", "Date", "Time"]
+        result = pd.DataFrame()
+        for filename in self.fnames:
+            try:
+                dataframe = pd.read_csv(filename, delimiter='\t', names=names)
+                dataframe["Level, cm"] = dataframe["Level, cm"].map(lambda x: float(x.rstrip(" cm")))
+                dataframe = dateConcat(dataframe)
+                result = result.append(dataframe, ignore_index = True)          
+            except:
+                pass
+        
+        return result
     
 """ Класс Главное Окно"""
 class MainWindow(QMainWindow):
@@ -92,8 +168,8 @@ class MainWindow(QMainWindow):
         self.log_press1 = Pressure()
         self.log_press2 = Pressure()
         self.log_press3 = Pressure()
-        self.list_temp = []
-        self.list_level = []
+        self.log_temp = Temperature()
+        self.log_level = Level()
         
     def initUI(self):
         # 'Add' buttons
@@ -142,6 +218,7 @@ class MainWindow(QMainWindow):
     
     def solve_clicked(self):
         self.button_Run.setChecked(False)
+        self.execData()
         self.__Log('button_solve.clicked')
     
     def save_clicked(self):
@@ -149,8 +226,7 @@ class MainWindow(QMainWindow):
         fname = QFileDialog.getSaveFileName(self, 'Save File', filter='Excel files (*.xlsx)')[0]
         if '.xlsx' not in fname:
             fname = fname + '.xlsx'
-        self.edit_result.setText('Saved: ' + fname)
-        
+        self.edit_save.setText('Saved: ' + fname)
         if (fname[0] != '.'):
             self.union.to_excel(fname)
         self.__Log('button_save.clicked')
@@ -177,22 +253,28 @@ class MainWindow(QMainWindow):
         self.progressBar.setValue(20)
         p3 = self.log_press3.GetDataFrame()
         self.progressBar.setValue(30)
-        p1p2 = pd.merge(p1, p2, on="Date/Time", how="outer")
+        t = self.log_temp.GetDataFrame()
+        self.progressBar.setValue(35)
+        l = self.log_level.GetDataFrame()
         self.progressBar.setValue(40)
+        p1p2 = pd.merge(p1, p2, on="Date/Time", how="outer")
+        self.progressBar.setValue(45)
         p = pd.merge(p1p2, p3, on="Date/Time", how="outer")
         self.progressBar.setValue(50)
-   
-        self.union = p
+        pt = pd.merge(p, t, on="Date/Time", how="outer")
+        self.progressBar.setValue(55)
+        self.union = pd.merge(pt, l, on="Date/Time", how="outer")
+        self.progressBar.setValue(60)
         cols = self.union.columns.tolist()
         cols = cols[1:2]+cols[:1]+cols[2:]
         self.union = self.union[cols]
         self.union.sort_values('Date/Time', inplace = True)
-        self.__Log(str(len(self.union)))
         self.progressBar.setValue(70)
         
         if len(self.union) > 0:
             self.button_Save.setEnabled(True)
-            self.FillTable(self.union, self.table_result)
+            model = PandasModel(self.union)
+            self.table_result.setModel(model)
         else:
             self.button_Save.setEnabled(False)
         
@@ -225,14 +307,14 @@ class MainWindow(QMainWindow):
         self.__Log('press3_add.clicked')
         
     def temp_add(self):
-        self.list_temp.append('bonk')
-        self.fill_edit(self.edit_temp, *self.list_temp)
+        self.log_temp.AddFName()
+        self.fill_edit(self.edit_temp, *self.log_temp.GetFNames())
         self.accessRun()
         self.__Log('temp_add.clicked')
         
     def level_add(self):
-        self.list_level.append('bonk')
-        self.fill_edit(self.edit_level, *self.list_level)
+        self.log_level.AddFName()
+        self.fill_edit(self.edit_level, *self.log_level.GetFNames())
         self.accessRun()
         self.__Log('level_add.clicked')
     
@@ -256,14 +338,14 @@ class MainWindow(QMainWindow):
         self.__Log('press3_clear.clicked')
         
     def temp_clear(self):
-        self.list_temp = []
-        self.fill_edit(self.edit_temp, *self.list_temp)
+        self.log_temp.ClearFNames()
+        self.fill_edit(self.edit_temp, *self.log_temp.GetFNames())
         self.accessRun()
         self.__Log('temp_clear.clicked')
         
     def level_clear(self):
-        self.list_level = []
-        self.fill_edit(self.edit_level, *self.list_level)
+        self.log_level.ClearFNames()
+        self.fill_edit(self.edit_level, *self.log_level.GetFNames())
         self.accessRun()
         self.__Log('level_clear.clicked')
 
@@ -274,8 +356,8 @@ class MainWindow(QMainWindow):
         a = self.log_press1.GetFNames() != []
         b = self.log_press2.GetFNames() != []
         c = self.log_press3.GetFNames() != []
-        d = self.list_temp != []
-        e = self.list_level != []
+        d = self.log_temp.GetFNames() != []
+        e = self.log_level.GetFNames() != []
         if a and b and c and d and e:
             self.button_Run.setEnabled(True)
             self.button_Stop.setEnabled(True)
